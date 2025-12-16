e47f0324620438d17e30e6bd780f38a2acab04f8
fouad lmrini <fouad11.lamrini@gmail.com>
2025-12-16 15:24:25 +0100

    ajouter route logoute

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,6 +5,8 @@ const controller = require("../controllers/auth.controller");
 
 router.post("/register", controller.register);
 router.post("/login", controller.login);
+router.post("/logout", controller.logout);
+
 
 
 module.exports = router;
