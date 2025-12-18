ec7e7317533365b53453d4099715843060ec6886
fouad lmrini <fouad11.lamrini@gmail.com>
2025-12-18 10:26:44 +0100

    route pour google auth

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const passport = require("../config/passport");
 const router = express.Router();
 const controller = require("../controllers/auth.controller");
 
@@ -7,6 +8,14 @@ router.post("/register", controller.register);
 router.post("/login", controller.login);
 router.post("/logout", controller.logout);
 
+// Google OAuth
+router.get("/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
+router.get("/auth/google/callback",
+  passport.authenticate("google", { session: false }),
+  authController.googleCallback
+);
 
 module.exports = router;
