b410cc650c33396710566b1f8e5e7f7c07e3f349
fouad lmrini <fouad11.lamrini@gmail.com>
2025-12-20 22:47:57 +0100

    ajouter roleRequest model

---
+++ src/models/RoleRequest.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const roleRequestSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    requestedRole: {
+      type: String,
+      enum: ["formateur", "etudiant"],
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "rejected"],
+      default: "pending",
+    },
+    reviewedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // admin
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("RoleRequest", roleRequestSchema);
