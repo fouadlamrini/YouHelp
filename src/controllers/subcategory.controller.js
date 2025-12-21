0d6053fbe8ead5ae77087129120a29c8685768d6
fouad lmrini <fouad11.lamrini@gmail.com>
2025-12-21 17:10:54 +0100

    create sub category

--- src/controllers/subcategory.controller.js
+++ src/controllers/subcategory.controller.js
@@ -15,4 +15,28 @@ class SubCategoryController {
       console.error(err);
       res.status(500).json({ message: "Server error" });
     }
-  }}
+  }
+
+// ================= CREATE =================
+async createSubCategory(req, res) {
+  try {
+    const { name, category } = req.body; 
+
+    const existingCategory = await Category.findOne({ name: category });
+
+    if (!existingCategory) {
+      return res.status(400).json({ message: "Category not found" });
+    }
+    const subCategory = await SubCategory.create({
+      name,
+      category: existingCategory._id,
+    });
+
+    res.status(201).json({ success: true, data: subCategory });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+}
+}
