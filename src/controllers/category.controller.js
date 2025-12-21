855e861ff9d55764c95ab5bec8ddcdb29b490a36
fouad lmrini <fouad11.lamrini@gmail.com>
2025-12-21 10:39:42 +0100

    creer category

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -10,5 +10,20 @@ class CategoryController {
       res.status(500).json({ message: 'Server error' });
     }
   }
-  
+  // CREATE new category
+  async createCategory(req, res) {
+    try {
+      const { name } = req.body;
+      const existing = await Category.findOne({ name });
+      if (existing) {
+        return res.status(400).json({ message: 'Category already exists' });
+      }
+
+      const category = await Category.create({ name });
+      res.status(201).json({ success: true, data: category });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: 'Server error' });
+    }
+  }
 }
