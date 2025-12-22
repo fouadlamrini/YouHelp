d3ce3ee375fad4a6609debb6ffb92140e8007461
fouad lmrini <fouad11.lamrini@gmail.com>
2025-12-22 17:31:52 +0100

    ajouter routes pour post

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,12 +1,35 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const PostController = require("../controllers/post.controller");
-const auth = require("../middlewares/auth.middleware");
+const PostController = require('../controllers/Post.controller');
+const auth = require('../middlewares/auth.middleware');
+const { requireRole } = require('../middlewares/role.middleware');
 
+/* ===== READ ===== */
 router.get("/", PostController.getAllPosts);
+router.get("/:id", PostController.getPostById);
 
-router.post("/", auth, PostController.createPost);
-router.put("/:id", auth, PostController.updatePost);
-router.delete("/:id", auth, PostController.deletePost);
+/* ===== CREATE (NO connected) ===== */
+router.post(
+  "/",
+  auth,
+  requireRole(["admin", "formateur", "etudiant"]),
+  PostController.createPost
+);
+
+/* ===== UPDATE (admin OR owner) ===== */
+router.put(
+  "/:id",
+  auth,
+  checkOwnerOrAdmin,
+  PostController.updatePost
+);
+
+/* ===== DELETE (admin OR owner) ===== */
+router.delete(
+  "/:id",
+  auth,
+  checkOwnerOrAdmin,
+  PostController.deletePost
+);
 
 module.exports = router;
