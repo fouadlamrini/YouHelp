08c5bf0d465624ba05dea46bd12c5a5ce9b3b124
fouad lmrini <fouad11.lamrini@gmail.com>
2025-12-23 11:06:59 +0100

    modifier sur route create post

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -4,7 +4,7 @@ const PostController = require('../controllers/post.controller');
 const auth = require('../middlewares/auth.middleware');
 const { requireRole } = require('../middlewares/role.middleware');
 const checkOwnerOrAdmin = require("../middlewares/checkOwnerOrAdmin.middleware");
-
+const upload = require('../middlewares/upload.middleware');
 
 /* ===== READ ===== */
 router.get("/", PostController.getAllPosts);
@@ -14,7 +14,8 @@ router.get("/", PostController.getAllPosts);
 router.post(
   "/",
   auth,
-  requireRole(["admin", "formateur", "etudiant"]),
+  requireRole(["admin","formateur","etudiant"]),
+  upload.array('media', 10), // permet d'ajouter 10 fichier
   PostController.createPost
 );
 
