460dcfa786d9bb47cb86d51480c5373d6ca0d984
fouad lmrini <fouad11.lamrini@gmail.com>
2026-01-03 19:44:12 +0100

    modifier app.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -5,6 +5,13 @@ import About from "./pages/About";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import AdminPage from "./pages/admin/AdminPage";
+import Users from "./pages/admin/Users";
+import Categories from "./pages/admin/Categories";
+import Subcategories from "./pages/admin/Subcategories";
+import Knowledge from "./pages/admin/Knowledge";
+import Posts from "./pages/admin/Posts";
+import Favorites from "./pages/admin/Favorites";
+import RequestRole from "./pages/admin/RequestRole";
 import ConnectedPage from "./pages/ConnectedPage";
 import ProtectedRoute from "./routes/ProtectedRoute";
 
@@ -18,7 +25,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
-        {/* Routes protégées */}
+        {/* Routes protégées admin */}
         <Route
           path="/admin"
           element={
@@ -26,8 +33,18 @@ function App() {
               <AdminPage />
             </ProtectedRoute>
           }
-        />
+        >
+          {/* Nested routes pour sidebar options */}
+          <Route path="users" element={<Users />} />
+          <Route path="categories" element={<Categories />} />
+          <Route path="subcategories" element={<Subcategories />} />
+          <Route path="knowledge" element={<Knowledge />} />
+          <Route path="posts" element={<Posts />} />
+          <Route path="favorites" element={<Favorites />} />
+          <Route path="requestrole" element={<RequestRole />} />
+        </Route>
 
+        {/* Routes protégées connected */}
         <Route
           path="/connected"
           element={
