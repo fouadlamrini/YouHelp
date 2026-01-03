d365019a4a4a52d720c2e4558ac068b6fee84e16
fouad lmrini <fouad11.lamrini@gmail.com>
2026-01-03 18:56:04 +0100

    page about

---
+++ front-end/src/pages/About.jsx
@@ -0,0 +1,23 @@
+export default function About() {
+  return (
+    <div className="pt-24 max-w-5xl mx-auto px-4">
+      <h1 className="text-4xl font-bold mb-6 text-center">About YouHelp</h1>
+
+      <p className="text-gray-700 text-lg mb-6">
+        YouHelp hiya plateforme mوجهة l apprenant dyal YouCode campus Nador،
+        bach ytsawlo, yta3awno, w yشاركوا knowledge binathom.
+      </p>
+
+      <p className="text-gray-700 text-lg mb-6">
+        projet kay3tamed 3la logique dyal communauté:
+        etudiant kayطرح mochkil, collegue expert wla formateur kayjawbo
+        b commentaire, message privé wla appel vidéo.
+      </p>
+
+      <p className="text-gray-700 text-lg">
+        مستقبلا, YouHelp t9dr tts3 f campus khrin b7al Youssoufia w Safi,
+        w tzid roles b7al Super Admin w RH.
+      </p>
+    </div>
+  );
+}
